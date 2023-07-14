07536e74aec74feb99cd09a3eba020f20ea43502
Yasen Kostov <ykostov@pm.me>
2023-07-14 14:57:23 +0300

    update url

--- app/src/main/java/net/bratac/friendczarapp/MainActivity.java
+++ app/src/main/java/net/bratac/friendczarapp/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mywebView=(WebView) findViewById(R.id.webview);
         mywebView.setWebViewClient(new WebViewClient());
-        mywebView.loadUrl("https://www.bratac.net/");
+        mywebView.loadUrl("https://www.chat.bratac.net/");
         WebSettings webSettings=mywebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
     }
